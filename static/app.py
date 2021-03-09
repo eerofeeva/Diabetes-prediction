1af00ccc13610b334ee18888a6359f0ee28df1c3
Elena Erofeeva <eerofeeva8@gmail.com>
2021-03-08 22:42:21 -0600

    WIP: added navbar, embedded JupyterNotebook html in iframe, added collapseable sections, still needs to be prettier

--- static/app.py
+++ static/app.py
@@ -1,22 +1,38 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from flask_bootstrap import Bootstrap
+import random
+
+class User:
+    def __init__(self, name, weight, height, glucose, active, skin, generate):
+        if generate == False:
+            self.name = name
+            self.weight = weight
+            self.height = height
+            self.BMI = self.calculateBMI
+            self.glucose = self.randomInRange(0, 199)
+            self.active = self.randomInRange(0,122)
+            self.skin = self.randomInRange(0, 99)
+        else:
+            self.generate()
 
 
-# class User:
-#     def __init__(name, weight, height, age ):
-#         self.name = name
-#         self.weight = weight
-#         self.height = height
-#         self.name = name
+    def calculateBMI(self):
+        return 703*self.height/self.weight**2   
 
-#     def calculateBMI():
-#         print("test bmi")    
+    def isLikely(self):
+        pass
 
-#     def isLikely():
-#         pass
-
-#     def generate():
-#         pass
+    def generate(self):
+        self.name = "sample user"
+        self.weight = self.randomInRange(120, 300)
+        self.height = self.randomInRange(60, 96)
+        self.BMI = self.calculateBMI
+        self.glucose = self.randomInRange(70, 250)
+        self.active = self.randomInRange(30,120)
+        self.skin = self.randomInRange(20, 70)
+    
+    def randomInRange(self, x,y):
+        return round(random.uniform(x,y), 3)    
 
 
 app=Flask(__name__)
@@ -28,12 +44,35 @@ def index():
     print("Running index function")
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return send_file("../Resources/about_section.txt")
+
 @app.route("/userUpload", methods=['GET','POST'])
 def userUpload():
-    theName = request.form.get('userName','')
-    print("userUpload: "+theName)
-    return theName
+    user = User(request.form.get('userName',''),
+    request.form.get('weight',''),
+    request.form.get('height',''),
+    request.form.get('sugar',''),
+    request.form.get('active',''),
+    request.form.get('skinThickness',''), False)
+    print("userUpload: " + user.name)
+    return user.name +' Weight: ' + str(user.weight) + ' Height: ' + str(user.height) \
+    + ' Glucose: ' + str(user.glucose) + ' Is Active: '+ str(user.active)  \
+    + ' Skin Thickness: ' + str(user.skin)
 
+@app.route("/userGenerate", methods=['GET','POST'])
+def userGenerate():
+    user = User(request.form.get('userName',''),
+    request.form.get('weight',''),
+    request.form.get('height',''),
+    request.form.get('sugar',''),
+    request.form.get('active',''),
+    request.form.get('skinThickness',''), True)
+    print("userUpload: " + user.name)
+    return user.name +' Weight: ' + str(user.weight) + ' Height: ' + str(user.height) \
+    + ' Glucose: ' + str(user.glucose) + ' Is Active: '+ str(user.active)  \
+    + ' Skin Thickness: ' + str(user.skin)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/static/app.js
+++ static/static/app.js
@@ -1,4 +1,45 @@
 $(document).ready(function() {
+    $("#btnGenerate").click(function (event) {
+ 
+        //stop submit the form, we will post it manually.
+        event.preventDefault();
+ 
+        // Get form
+        var form = $('#userUpload')[0];
+ 
+       // Create an FormData object 
+        var data = new FormData(form);
+ 
+       // disabled the submit button
+        $("#btnGenerate").prop("disabled", true);
+ 
+        $.ajax({
+            type: "POST",
+            enctype: 'multipart/form-data',
+            url: "/userGenerate",
+            data: data,
+            processData: false,
+            contentType: false,
+            cache: false,
+            timeout: 800000,
+            success: function (data) {
+ 
+                $("#output").text(data);
+                console.log("SUCCESS : ", data);
+                $("btnGenerate").prop("disabled", false);
+ 
+            },
+            error: function (e) {
+ 
+                $("#output").text(e.responseText);
+                console.log("ERROR : ", e);
+                $("#btnGenerate").prop("disabled", false);
+ 
+            }
+        });
+    });
+
+
     $("#btnSubmit").click(function (event) {
  
         //stop submit the form, we will post it manually.
